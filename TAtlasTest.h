69371ca03e164b6af014b6f50968200456b9651e
linkv <linkv@biolpc55.unifr.ch>
2018-01-22 18:21:20 +0100

    started adding recal test

--- TAtlasTest.h
+++ TAtlasTest.h
@@ -68,42 +68,6 @@ public:
 	bool run();
 };
 
-//------------------------------------------
-//TAtlasTest_BQSRSimulation
-//------------------------------------------
-class TAtlasTest_BQSRSimulation:public TAtlasTest{
-private:
-	std::string filenameTag;
-	std::string bamFileName;
-	std::string fastaFileName;
-	int meanQual;
-	double sdphredInt;
-	int minPhredInt;
-	int maxPhredInt;
-	int phi1;
-	double phi2;
-	double revIntercept;
-	double acceptedDelta;
-	std::string qualityDist;
-//	double alpha;
-//	double beta;
-	int minReadLen;
-	int maxReadLen;
-//	std::string readLengthDist;
-	double positionEffectSlope;
-	double positionEffectIntercept;
 
-
-public:
-	TAtlasTest_BQSRSimulation(TParameters & params, TLog* logfile);
-	~TAtlasTest_BQSRSimulation(){};
-	bool run();
-	double trueQual(int & phi1, double & phi2, int & fakeQual);
-	bool checkBQSRQualityFile();
-	bool checkBQSRPositionFile();
-	void calculateSlopeIntercept();
-	double trueScaling(int & pos);
-
-};
 
 #endif /* TATLASTEST_H_ */
