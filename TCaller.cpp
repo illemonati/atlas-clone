8e67d371ade1cf4457ae6f46e1365ba371845ae0
Dan Wegmann <daniel.wegmann@unifr.ch>
2018-11-27 14:30:40 +0100

    Fixed bug adding alternative allele twice in case secon dmost likely genotype was also homozygous.

--- TCaller.cpp
+++ TCaller.cpp
@@ -554,13 +554,16 @@ void TCallerDiploid::callGenotypeFromMetric(double* metric){
 				altAlleles.push_back(genoMap.genotypeToBase[indexOfMax][0]);
 				calledGenotype = "1/1";
 
+
+
 				//find second best allele, but give preference to reference in case likelihoods are equal
 				int hetRef = genoMap.getGenotype(referenceBase, genoMap.genotypeToBase[indexOfMax][0]);
-				if(_allowTriallelicSites && metric[hetRef] < metric[indexOfSecond]){
+				int homRef = genoMap.getGenotype(referenceBase, referenceBase);
+				if(_allowTriallelicSites && metric[hetRef] < metric[indexOfSecond] && metric[homRef] < metric[indexOfSecond]){
 					//only use second alternative allele in case het genotype with reference is less likely
 					if(genoMap.genotypeToBase[indexOfSecond][0] != referenceBase && genoMap.genotypeToBase[indexOfSecond][0] != altAlleles[0])
 						altAlleles.push_back(genoMap.genotypeToBase[indexOfSecond][0]);
-					if(genoMap.genotypeToBase[indexOfSecond][1] != referenceBase && genoMap.genotypeToBase[indexOfSecond][1] != altAlleles[0])
+					if(genoMap.genotypeToBase[indexOfSecond][1] != referenceBase && genoMap.genotypeToBase[indexOfSecond][1] != altAlleles[0] && genoMap.genotypeToBase[indexOfSecond][1] != altAlleles[1])
 						altAlleles.push_back(genoMap.genotypeToBase[indexOfSecond][1]);
 				}
 			} else {
