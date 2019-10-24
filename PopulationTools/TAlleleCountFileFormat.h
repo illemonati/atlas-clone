13897f699ca3cdd11e2acb0678f20a9fb148f4e3
vlink <vivian.link@sunrise.ch>
2019-10-24 09:30:41 +0200

    class for alleleCount output formats

---
+++ PopulationTools/TAlleleCountFileFormat.h
@@ -0,0 +1,21 @@
+/*
+ * TAlleleCountFileFormat.h
+ *
+ *  Created on: Oct 23, 2019
+ *      Author: vivian
+ */
+
+#ifndef POPULATIONTOOLS_TALLELECOUNTFILEFORMAT_H_
+#define POPULATIONTOOLS_TALLELECOUNTFILEFORMAT_H_
+
+class TAlleleCountFile{
+private:
+
+public:
+	TAlleleCountFile();
+	write();
+};
+
+
+
+#endif /* POPULATIONTOOLS_TALLELECOUNTFILEFORMAT_H_ */
