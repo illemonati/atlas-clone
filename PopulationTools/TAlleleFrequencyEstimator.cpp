d2de7b750c202336e75b99faa590704feeb0c6d7
Daniel Wegmann <daniel.wegmann@unifr.ch>
2019-11-04 10:30:57 +0100

    Much improved estimation of Bayesian allele frequencies

--- PopulationTools/TAlleleFrequencyEstimator.cpp
+++ PopulationTools/TAlleleFrequencyEstimator.cpp
@@ -69,27 +69,62 @@ double TAlleleFreqEstimatorHardyWeinberg::estimate(TPopulationLikehoodLocus & st
 TAlleleFreqEstimatorBayes::TAlleleFreqEstimatorBayes(TParameters & Parameters, TLog* logfile, TRandomGenerator* RandomGenerator){
 	randomGenerator = RandomGenerator;
 	logfile->startIndent("Initializing Bayesian allele frequency estimator:");
-	double mcmcLength = Parameters.getParameterIntWithDefault("mcmcLength", 50000);
-	logfile->list("Will run MCMC chains of length " + toString(mcmcLength) + ".");
-	numBurnins = Parameters.getParameterIntWithDefault("numBurnins", 5);
-	burninLength = Parameters.getParameterIntWithDefault("burninLength", 2000);
-	logfile->list("Will run " + toString(numBurnins) + " burnins of length " + toString(burninLength) + " each.");
 
+	//prior
 	alpha = Parameters.getParameterDoubleWithDefault("alpha", 0.5);
 	beta = Parameters.getParameterDoubleWithDefault("beta", 0.5);
 	alphaMinusOne = alpha - 1.0;
 	betaMinusOne = beta - 1.0;
-	logfile->list("Will use a beta(" + toString(alpha) + "," + toString(beta) + ") prior.");
+	logfile->list("Will use a beta(" + toString(alpha) + "," + toString(beta) + ") prior (alpha, beta).");
 	logfile->endIndent();
 
-	//initialize
-	mcmc.resize(mcmcLength);
-	lowerIndex = floor(mcmcLength * 0.05);
-	upperIndex = ceil(mcmcLength * 0.95);
-	f = 0.5;
-	f_lower = 0.0;
-	f_upper = 1.0;
-	acceptanceRate = 0.0;
+	//MAP estimation
+	numMAPSIterations = Parameters.getParameterIntWithDefault("MAPIterations", 100);
+	logfile->list("Will search for the MAP using ", numMAPSIterations, " iterations (MAPIterations).");
+	f_MAP = 0.5;
+	f_CI_lower = 0.0;
+	f_CI_upper = 1.0;
+	LL_atMAP = 0.0;
+
+	//prepare initial search grid between 0.0 and 1.0
+	credibleInterval = Parameters.getParameterDoubleWithDefault("credibleInterval", 0.9);
+	logfile->list("Will calculate the ", credibleInterval, " Credible Interval (credibleInterval).");
+	initialGridSize = Parameters.getParameterIntWithDefault("initialGridSize", 101);
+	logfile->list("Will use an initial grid of size ", initialGridSize, " to identify relevant frequency range (initialGridSize).");
+	if(initialGridSize < 3){
+		throw "Initial grid size must be >= 3!";
+	}
+	initialGridLast = initialGridSize - 1;
+	LL_initialGrid = new double[initialGridSize];
+	f_initialGrid = new double[initialGridSize];
+	double step = 1.0 / (double) initialGridLast;
+	f_initialGrid[0] = 0.0;
+	for(int i=1; i<initialGridLast; ++i){
+		f_initialGrid[i] = i * step;
+	}
+	f_initialGrid[initialGridLast] = 1.0;
+
+	//final grid
+	gridSize = Parameters.getParameterIntWithDefault("gridSize", 1001);
+	logfile->list("Will use a grid of size ", gridSize, " to calculate credible interval (gridSize).");
+	if(gridSize < 10){
+		throw "Initial grid size must be >= 10!";
+	}
+	gridLast = gridSize - 1;
+	logGridThreshold = Parameters.getParameterDoubleWithDefault("logGridThreshold", 14.0);
+	logfile->list("Will use a threshold ", logGridThreshold, " to span the grid (logGridThreshold).");
+	if(logGridThreshold < 1.0){
+		throw "grid threshold must be >= 1.0!";
+	}
+	Likelihood_grid = new double[gridSize];
+	f_grid = new double[gridSize];
+};
+
+TAlleleFreqEstimatorBayes::~TAlleleFreqEstimatorBayes(){
+	delete[] f_initialGrid;
+	delete[] LL_initialGrid;
+	delete[] f_grid;
+	delete[] Likelihood_grid;
 };
 
 double TAlleleFreqEstimatorBayes::guessInitialAlleleFrequency(TPopulationLikehoodLocus & storage, TGlfConverter & glfConverter){
@@ -134,77 +169,135 @@ double TAlleleFreqEstimatorBayes::calcLL(TPopulationLikehoodLocus & storage, THa
 	return LL;
 };
 
-int TAlleleFreqEstimatorBayes::makeMCMCUpdate(TPopulationLikehoodLocus & storage, double & oldLL, const double & prop, THardyWeinbergGenotypeProbabilities* pGenotype, int & old, TGlfConverter & glfConverter){
-	int cur = 1-old;
-
-	//propose new f
-	double new_f = fabs(pGenotype[old].f + randomGenerator->getRand() * prop - prop / 2.0);
-	if(new_f > 1.0) new_f = 2.0 - new_f;
-	pGenotype[cur].set(new_f);
-
-	//calc hastings
-	double LL = calcLL(storage, pGenotype[cur], glfConverter);
-	double priorRatio = alphaMinusOne * log(pGenotype[cur].f / pGenotype[old].f) + betaMinusOne * log(pGenotype[old].oneMinusf / pGenotype[old].oneMinusf);
-	double hastings = LL - oldLL + priorRatio;
-
-	//accept or reject
-	if(log(randomGenerator->getRand()) < hastings){
-		old = cur;
-		oldLL = LL;
-		return true;
+void TAlleleFreqEstimatorBayes::fillInitialGrid(TPopulationLikehoodLocus & storage, TGlfConverter & glfConverter){
+	for(int i=0; i<initialGridSize; ++i){
+		pGenotype.set(f_initialGrid[i]);
+		LL_initialGrid[i] = calcLL(storage, pGenotype, glfConverter);
 	}
-	return false;
+};
+
+void TAlleleFreqEstimatorBayes::estimateMAP(TPopulationLikehoodLocus & storage, TGlfConverter & glfConverter){
+	//use simple line-search to find MAP
+	//initialize
+	pGenotype.set(guessInitialAlleleFrequency(storage, glfConverter));
+	LL_atMAP = calcLL(storage, pGenotype, glfConverter);
+	double step = 0.05;
+
+	//now do line search
+	for(int i=0; i<numMAPSIterations; ++i){
+		//propose new f
+		pGenotype.set(pGenotype.f + step);
+
+		//calc LL and switch if LL is lower
+		double LL = calcLL(storage, pGenotype, glfConverter);
+		if(LL < LL_atMAP || pGenotype.f == 0.0 || pGenotype.f == 1.0){
+			step = - step / 2.718282;
+		}
+		LL_atMAP = LL;
+	}
+	f_MAP = pGenotype.f;
+
+	//assumes that initial grid was pre-calculated! to check and boundaries
+	fillInitialGrid(storage, glfConverter);
+
+	//check if MAP is zero or one
+	if(LL_initialGrid[0] >= LL_atMAP){
+		f_MAP = 0.0;
+		LL_atMAP = LL_initialGrid[0];
+	} else if(LL_initialGrid[initialGridLast] >= LL_atMAP){
+		f_MAP = 1.0;
+		LL_atMAP = LL_initialGrid[initialGridLast];
+	}
+};
+
+void TAlleleFreqEstimatorBayes::estimateCredibleIntervals(TPopulationLikehoodLocus & storage, TGlfConverter & glfConverter){
+	//use initial grid to define final grid
+	//search first that is larger than LL_atMAP - logGridThreshold
+	int first = 0;
+	double relevantLL = LL_atMAP - logGridThreshold;
+	while(LL_initialGrid[first] < relevantLL && f_initialGrid[first] < f_MAP){
+		++first;
+	}
+	if(first > 0) --first;
+
+	//search last
+	int last = initialGridLast;
+	while(LL_initialGrid[last] < relevantLL && f_initialGrid[last] > f_MAP){
+		--last;
+	}
+	if(last < initialGridLast) ++last;
+
+	//now prepare grid and calculate LL
+	//make sure none is > LL_atMAP
+	double step = (f_initialGrid[last] - f_initialGrid[first]) / (double) (gridLast);
+	double integral = 0.0;
+	for(int i=0; i<gridSize; ++i){
+		f_grid[i] = f_initialGrid[first] + i * step;
+		pGenotype.set(f_grid[i]);
+		Likelihood_grid[i] = exp(calcLL(storage, pGenotype, glfConverter) - LL_atMAP);
+		integral += Likelihood_grid[i];
+	}
+
+	//adjust integral: remove half of first and last and multiply by step
+	integral -= (Likelihood_grid[0] + Likelihood_grid[gridLast]) / 2.0; //first and last count half a step
+	integral *= step;
+
+	//find index left and right of MAP
+	int left = gridLast;
+	while(f_grid[left] >= f_MAP && left > 0){
+		--left;
+	}
+
+	int right = 0;
+	while(f_grid[right] <= f_MAP && right < gridLast){
+		++right;
+	}
+
+	//add part from MAP to left and right grid points: average height as used when computing integral
+	//then move left and right to next ones
+	double halfStep = step / 2.0;
+	double CI = (Likelihood_grid[left] + Likelihood_grid[right])  * halfStep;
+	-- left;
+	++right;
+
+	//now find 90% CI by iteratively adding on left and right of MAP, depending on which has higher LL
+	double relevantIntegral = credibleInterval * integral;
+	while(CI < relevantIntegral){
+		if(left < 0 || Likelihood_grid[left] < Likelihood_grid[right]){
+			//add at right
+			double add = (Likelihood_grid[right] + Likelihood_grid[right - 1]) * halfStep;
+			if(CI + add > relevantIntegral){
+				f_CI_lower = f_grid[left + 1];
+				f_CI_upper = f_grid[right - 1] + step * (relevantIntegral - CI) / add;
+				break;
+			} else {
+				CI += add;
+				++right;
+			}
+		} else {
+			//add at left
+			double add = (Likelihood_grid[left] + Likelihood_grid[left + 1]) * halfStep;
+			if(CI + add > relevantIntegral){
+				f_CI_upper = f_grid[right - 1];
+				f_CI_lower = f_grid[left + 1] + step * (relevantIntegral - CI) / add;
+				break;
+			} else {
+				CI += add;
+				--left;
+			}
+		}
+	}
 };
 
 double TAlleleFreqEstimatorBayes::estimate(TPopulationLikehoodLocus & storage, TGlfConverter & glfConverter){
-	//initialize
-	THardyWeinbergGenotypeProbabilities pGenotype[2];
-	int old = 0;
-	pGenotype[old].set(guessInitialAlleleFrequency(storage, glfConverter));
-	double prop = pGenotype[old][1];
-	if(prop > 0.5) prop = 0.5;
+	//get MAP estimate
+	estimateMAP(storage, glfConverter);
 
-	//calc initial LL
-	double oldLL = calcLL(storage, pGenotype[old], glfConverter);
+	//estimate credible interval
+	estimateCredibleIntervals(storage, glfConverter);
 
-	//run burnins
-	for(int b=0; b<numBurnins; b++){
-		int acceptance = 0;
-		for(int i=0; i<burninLength; i++){
-			acceptance += makeMCMCUpdate(storage, oldLL, prop, pGenotype, old, glfConverter);
-		}
-
-		//adjust proposal range
-
-		prop = prop * 3.0 * (double) (acceptance+1) / (double) (burninLength+1);
-		if(prop > 0.5) prop = 0.5;
-	}
-
-	//run MCMC
-	int acceptance = 0;
-	for(size_t i=0; i<mcmc.size(); i++){
-		acceptance += makeMCMCUpdate(storage, oldLL, prop, pGenotype, old, glfConverter);
-
-		//store current f
-		mcmc[i] = pGenotype[1-old].f;
-	}
-	acceptanceRate = (double) acceptance / (double) mcmc.size();
-
-	//std::cout << "Acceptance = " << (double) acceptance / (double) mcmc.size() << ", prop = " << prop << std::endl;
-
-	//estimate posterior mean
-	double f = 0.0;
-	for(const double& sample : mcmc){
-		f += sample;
-	}
-	f /= (double) mcmc.size();
-
-	//estimate posterior quantiles
-	std::sort(mcmc.begin(), mcmc.end());
-	f_lower = mcmc[lowerIndex];
-	f_upper = mcmc[upperIndex];
-
-	return f;
+	//return MAP estimate
+	return f_MAP;
 };
 
 
@@ -271,10 +364,9 @@ void TAlleleFreqEstimator::estimateAlleleFreq(TParameters & Parameters, TRandomG
 	//write header
 	std::vector<std::string> header = {"chr", "pos", "ref", "alt", "numDiploid", "numHaploid", "freqAltHW", "freqGenoRefRef", "freqGenoRefAlt", "freqGenoAltAlt", "freqGenoRef", "freqGenoAlt", "freqAltGF"};
 	if(doBayesian){
-		header.push_back("freqAltHWBayes");
-		header.push_back("freqAltHWBayes_CI0.05");
-		header.push_back("freqAltHWBayes_CI0.95");
-		header.push_back("MCMCAcceptance");
+		header.push_back("freqAltHW_MAP");
+		header.push_back("freqAltHW_CI" + toString(BHWEstimator->credibleIntervalUsed()) + "_lower");
+		header.push_back("freqAltHW_CI" + toString(BHWEstimator->credibleIntervalUsed()) + "_upper");
 	}
 	out.writeHeader(header);
 
@@ -300,12 +392,9 @@ void TAlleleFreqEstimator::estimateAlleleFreq(TParameters & Parameters, TRandomG
 
  		//Bayesian estimation
  		if(doBayesian){
- 			//std::cout << reader.position() << "\t";
-
  			out << BHWEstimator->estimate(storage, glfConverter);
  			out << BHWEstimator->lowerCredibleInterval();
  			out << BHWEstimator->upperCredibleInterval();
- 			out << BHWEstimator->mcmcAcceptance();
  		};
 
  		//end line
@@ -318,6 +407,9 @@ void TAlleleFreqEstimator::estimateAlleleFreq(TParameters & Parameters, TRandomG
     //clean up
 	vcfRead = true;
 	out.close();
+	if(doBayesian){
+		delete BHWEstimator;
+	}
 
     //report final status
 	logfile->endIndent();
