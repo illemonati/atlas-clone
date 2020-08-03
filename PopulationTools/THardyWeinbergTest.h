f0d11af9d37016a2912e45e6d339279ddce18074
Daniel Wegmann <daniel.wegmann@unifr.ch>
2020-08-03 09:49:50 +0200

    Working on HW test

--- PopulationTools/THardyWeinbergTest.h
+++ PopulationTools/THardyWeinbergTest.h
@@ -12,8 +12,51 @@
 #include "TPopulationLikelihoods.h"
 #include "TQualityMap.h"
 #include "TRandomGenerator.h"
+#include "mathFunctions.h"
 
 namespace PopulationTools{
+
+
+
+//------------------------------------------------
+//THWHetProbDB
+// Stores the probabilities P(N_AB = n|N, n_A, n_B),
+// i.e the probabilities of observing n hets among N individuals if the allele frequencies are n_A + n_B = N
+//------------------------------------------------
+class THWHetProb{
+private:
+	uint32_t _maxNumHet;
+
+	bool _odd; //indicates whether the numbe ro fpossible hets are even or odd
+	std::vector<double> _probs;
+
+public:
+	THWHetProb(const uint32_t & N, const uint32_t & n_A);
+
+	const uint32_t& maxNumHet() const { return _maxNumHet; };
+	const bool& odd() const { return _odd; };
+	const double& operator[](const uint32_t & i) const { return _probs[i]; };
+};
+
+
+class THWHetProbVector{
+private:
+	uint32_t _N;
+	std::map<uint32_t, THWHetProb> _probs;
+
+public:
+	THWHetProbVector(const uint32_t & N);
+	const THWHetProb& getProbs(const uint32_t & n_A);
+};
+
+class THWProbDB{
+private:
+	std::map<uint32_t, THWHetProbVector> _probs;
+
+public:
+	const THWHetProb& getProbs(const uint32_t & N, const uint32_t & n_A);
+};
+
 
 //------------------------------------------------
 //THWGenotypes
@@ -25,8 +68,26 @@ private:
 public:
 	THWGenotypes(){ clear(); };
 
-	void clear(){ _genoCounts.fill(0); };
-	void add(uint8_t genotype){ ++_genoCounts[genotype]; };
+	void clear();
+	void add(const uint8_t & genotype);
+	uint32_t N();
+	uint32_t n_A();
+};
+
+//------------------------------------------------
+//THWProbsMultiPop
+//------------------------------------------------
+class THWProbsMultiPop{
+private:
+	std::vector<double> _probs;
+	uint32_t _maxNumHet;
+	uint32_t _curMaxNumHetPlusOne;
+
+public:
+	THWProbsMultiPop();
+
+	void initialize(const uint32_t & maxNumHet, const THWHetProb & probs);
+	void extend(const THWHetProb & probs);
 };
 
 //------------------------------------------------
@@ -35,21 +96,16 @@ public:
 class THWPopulations{
 private:
 	std::vector<THWGenotypes> _populations;
+	THWProbDB _probDB;
 
 public:
-	THWPopulations(uint16_t numPops){
-		_populations.resize(numPops);
-	};
+	THWPopulations(){};
+	THWPopulations(const uint16_t & numPops);
+	void clear();
+	void resize(const uint16_t & numPops);
+	void add(const uint16_t & pop, const uint8_t & genotyp);
 
-	void clear(){
-		for(auto& p : _populations){
-			p.clear();
-		}
-	};
-
-	void add(uint16_t pop, uint8_t genotyp){
-		_populations[pop].add(genotyp);
-	};
+	void runTest();
 };
 
 //------------------------------------------------
