a0c91bb3f7fc8fb05170a48944f52d95e5cc4e96
Andreas <andreas.fueglistaler@protonmail.com>
2022-01-25 15:46:04 +0100

    minor changes to SFS

--- Simulations/SFS.cpp
+++ Simulations/SFS.cpp
@@ -9,146 +9,116 @@
 
 namespace Simulations {
 //--------------------------------
-//Class to store and SFS
+// Class to store and SFS
 //--------------------------------
 
-SFS::SFS(const std::string & filename){
+SFS::SFS(const std::string &filename) {
 	std::ifstream file(filename.c_str());
-	if(!file)
-		throw "Failed to open SFS file '" + filename + "'!";
+	if (!file) throw "Failed to open SFS file '" + filename + "'!";
 	std::vector<double> vec;
 	coretools::str::fillContainerFromLineWhiteSpace(file, vec, true, true);
 
-	//now store as fraction
+	// now store as fraction
 	float sum = 0;
 	for (size_t i = 0; i < vec.size(); ++i) {
 		sfs.push_back(vec[i]);
 		sum += vec[i];
 	}
-	for (auto& s: sfs) s /= sum;
+	for (auto &s : sfs) s /= sum;
 	_fillFrequencies();
 	_fillCumulative();
 }
 
-SFS::SFS(SFS* other, float MonoFrac){
-	//construct from other SFS, but rescale SFS to have a specific fraction of monomorphic sites
-	//now copy and rescale
+SFS::SFS(const SFS &other, float MonoFrac) {
+	// construct from other SFS, but rescale SFS to have a specific fraction of monomorphic sites
+	// now copy and rescale
 	float sum = 0.0;
 	sfs.push_back(MonoFrac);
-	for(uint32_t i=1; i<other->sfs.size(); ++i){
-		sfs.push_back(other->sfs[i]);
-		sum += other->sfs[i];
+	for (uint32_t i = 1; i < other.sfs.size(); ++i) {
+		sfs.push_back(other.sfs[i]);
+		sum += other.sfs[i];
 	}
 	sum = sum / (1.0 - MonoFrac);
-	for (auto& s: sfs) s /= sum;
+	for (auto &s : sfs) s /= sum;
 	_fillFrequencies();
 	_fillCumulative();
-};
+}
 
-SFS::SFS(uint32_t numChr, float theta){
-	//generate sfs from theta
+SFS::SFS(uint32_t numChr, float theta) {
+	// generate sfs from theta
 	float sum = 0;
-	for(uint32_t i=1; i<numChr + 1; ++i){
+	for (uint32_t i = 1; i < numChr + 1; ++i) {
 		sfs.push_back(theta / i);
 		sum += sfs.back();
 	}
-	if(sum > 1.0)
-		throw "The choice of theta and sample size results in too many mutations in the SFS!";
+	if (sum > 1.0) throw "The choice of theta and sample size results in too many mutations in the SFS!";
 
 	sfs.front() = 1.0 - sum;
 	_fillFrequencies();
 	_fillCumulative();
-};
+}
 
-SFS::SFS(uint32_t numChr, uint32_t onlyThisBin){
-	//set all to zero except this one bin
+SFS::SFS(uint32_t numChr, uint32_t onlyThisBin) {
+	// set all to zero except this one bin
 	sfs.resize(numChr + 1, 0.);
 	sfs[onlyThisBin] = 1.0;
 	_fillFrequencies();
 	_fillCumulative();
-};
+}
 
-void SFS::_fillFrequencies(){
+void SFS::_fillFrequencies() {
 	sfsFrequencies[0] = 0.0;
-	for(uint32_t i=1; i<sfs.size(); ++i){
-		sfsFrequencies.push_back((float) i / (float) numChromosomes());
-	}
-};
+	for (uint32_t i = 1; i < sfs.size(); ++i) sfsFrequencies.push_back((float)i / numChromosomes());
+}
 
-void SFS::_fillCumulative(){
+void SFS::_fillCumulative() {
 	sfsCumulative[0] = sfs[0];
-	for(uint32_t i=1; i<sfs.size(); ++i){
-		sfsCumulative.push_back(sfsCumulative[i-1] + sfs[i]);
-	}
+	for (uint32_t i = 1; i < sfs.size(); ++i) sfsCumulative.push_back(sfsCumulative[i - 1] + sfs[i]);
 	sfsCumulative.back() = 1.0;
 }
 
-void SFS::writeToFile(const std::string & filename, const bool & writeLog){
+void SFS::writeToFile(const std::string &filename, const bool &writeLog) {
 	std::ofstream out(filename.c_str());
-	if(!out)
-		throw "Failed to open file '" + filename + "' for writing!";
+	if (!out) throw "Failed to open file '" + filename + "' for writing!";
 
-	if(writeLog){
+	if (writeLog) {
 		out << log(sfs[0]);
-		for(uint32_t i=1; i<sfs.size(); ++i){
-			out << " " << log(sfs[i]);
-		}
+		for (uint32_t i = 1; i < sfs.size(); ++i) { out << " " << log(sfs[i]); }
 	} else {
 		out << sfs[0];
-		for(uint32_t i=1; i<sfs.size(); ++i){
-			out << " " << sfs[i];
-		}
+		for (uint32_t i = 1; i < sfs.size(); ++i) { out << " " << sfs[i]; }
 	}
 	out << "\n";
 	out.close();
-};
-
-double SFS::calcLLOneSite(const std::vector<float> & gl){
-	double LL = 0.0;
-	for(uint32_t i=0; i<sfs.size(); ++i){
-		LL += exp(gl[i]) * sfs[i];
-	}
-
-	return log(LL);
-};
-
-double SFS::getRandomFrequency(coretools::TRandomGenerator* randomGenerator){
-	return sfsFrequencies[randomGenerator->pickOne(sfsCumulative)];
-};
-
-uint32_t SFS::getRandomAlleleCount(coretools::TRandomGenerator* randomGenerator){
-	return randomGenerator->pickOne(sfsCumulative);
-};
-
-//--------------------------------------
-//SFSfolded
-//--------------------------------------
-SFSfolded::SFSfolded(uint32_t numChr, float theta) {
-	//generate sfs from theta
-	float sum = 0;
-	for(uint32_t i=1; i<numChr + 1; ++i){
-		sfs.push_back(theta/i + theta/(numChromosomes() - i));
-		sum += sfs.back();
-	}
-	if(sum > 1.0)
-		throw "The choice of theta and sample size results in too many mutations in the SFS!";
-
-	sfs[0] = 1.0 - sum;
-	_fillFrequencies();
-	_fillCumulative();
-};
-
-double SFSfolded::calcLLOneSite(const std::vector<float>& gl){
-	const auto dimension = sfs.size();
-	double LL = 0.0;
-	for(uint32_t i=0; i<dimension; ++i)
-		LL += exp(gl[i]) * sfs[i];
-
-	int j = dimension-1;
-	for(uint32_t i=dimension; i<2 * dimension - 1; ++i, --j)
-		LL += exp(gl[i]) * sfs[j];
-
-	return log(LL);
-};
-
 }
+
+double SFS::calcLLOneSite(const std::vector<float> &gl) {
+	double LL = 0.0;
+	for (uint32_t i = 0; i < sfs.size(); ++i) LL += exp(gl[i]) * sfs[i];
+	return log(LL);
+}
+
+double SFS::getRandomFrequency(coretools::TRandomGenerator *randomGenerator) {
+	return sfsFrequencies[randomGenerator->pickOne(sfsCumulative)];
+}
+
+uint32_t SFS::getRandomAlleleCount(coretools::TRandomGenerator *randomGenerator) {
+	return randomGenerator->pickOne(sfsCumulative);
+}
+
+//--------------------------------------
+// SFSfolded
+//--------------------------------------
+
+double SFSfolded::calcLLOneSite(const std::vector<float> &gl) {
+	const auto dimension = sfs.size();
+	double LL            = 0.0;
+	for (uint32_t i = 0; i < dimension; ++i) LL += exp(gl[i]) * sfs[i];
+
+	int j = dimension - 1;
+	for (uint32_t i = dimension; i < 2 * dimension - 1; ++i, --j) LL += exp(gl[i]) * sfs[j];
+
+	return log(LL);
+}
+
+} // namespace Simulations
--- Simulations/SFS.h
+++ Simulations/SFS.h
@@ -8,47 +8,47 @@
 #ifndef SFS_H_
 #define SFS_H_
 
-#include <iostream>
-#include "stringFunctions.h"
-#include <math.h>
 #include "TRandomGenerator.h"
+#include "stringFunctions.h"
+#include <iostream>
+#include <math.h>
 
 namespace Simulations {
 //--------------------------------
-//Class to store and SFS
+// Class to store and SFS
 //--------------------------------
-class SFS{
+class SFS {
 protected:
 	void _fillFrequencies();
 	void _fillCumulative();
 	SFS() = default;
+
 public:
 	std::vector<float> sfs;
 	std::vector<float> sfsCumulative;
 	std::vector<float> sfsFrequencies;
 
-	SFS(const std::string & filename);
-	SFS(SFS* other, float MonoFrac);
+	SFS(const std::string &filename);
+	SFS(const SFS &other, float MonoFrac);
 	SFS(uint32_t numChr, float theta);
 	SFS(uint32_t numChr, uint32_t onlyThisBin);
 	virtual ~SFS() = default;
 
-	virtual uint32_t numChromosomes() const noexcept {return sfs.size() - 1;};
-	float monoFrac() const noexcept {return sfs.front();};
-	void writeToFile(const std::string & filename, const bool & writeLog=false);
-	double getRandomFrequency(coretools::TRandomGenerator* randomGenerator);
-	uint32_t getRandomAlleleCount(coretools::TRandomGenerator* randomGenerator);
-	virtual double calcLLOneSite(const std::vector<float>& gl);
+	virtual uint32_t numChromosomes() const noexcept { return sfs.size() - 1; };
+	float monoFrac() const noexcept { return sfs.front(); };
+	void writeToFile(const std::string &filename, const bool &writeLog = false);
+	double getRandomFrequency(coretools::TRandomGenerator *randomGenerator);
+	uint32_t getRandomAlleleCount(coretools::TRandomGenerator *randomGenerator);
+	virtual double calcLLOneSite(const std::vector<float> &gl);
 };
 
-class SFSfolded:public SFS{
+class SFSfolded : public SFS {
 public:
-	virtual uint32_t numChromosomes() const noexcept override {return 2*sfs.size() - 2;};
-	SFSfolded(const std::string & filename):SFS(filename){};
-	SFSfolded(SFSfolded* other, float MonoFrac):SFS(other, MonoFrac){};
-	SFSfolded(uint32_t numChr, float theta);
-	~SFSfolded(){};
-	double calcLLOneSite(const std::vector<float>& gl) override;
+	virtual uint32_t numChromosomes() const noexcept override { return 2 * sfs.size() - 2; };
+	SFSfolded(const std::string &filename) : SFS(filename){};
+	SFSfolded(const SFSfolded &other, float MonoFrac) : SFS(other, MonoFrac){};
+	SFSfolded(uint32_t numChr, float theta) :SFS(numChr, theta){}
+	double calcLLOneSite(const std::vector<float> &gl) override;
 };
 
 } // namespace Simulations
