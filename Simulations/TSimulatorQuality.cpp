35776899dae4db9c14e8ec0e322b11ea5dd0325d
Andreas <andreas.fueglistaler@protonmail.com>
2022-01-25 16:40:06 +0100

    minor changes

--- Simulations/TSimulatorQuality.cpp
+++ Simulations/TSimulatorQuality.cpp
@@ -7,219 +7,200 @@
 
 #include "TSimulatorQuality.h"
 #include "algorithmsAndVectors.h"
+#include "mathFunctions.h"
 
-namespace Simulations{
+namespace Simulations {
 
 //----------------------------------
-//TSimulatorQualityDist
+// TSimulatorQualityDist
 //----------------------------------
-TSimulatorQualityDist::TSimulatorQualityDist(std::string & s, TRandomGenerator* RandomGenerator){
-	size_t pos = s.find("(");
-	if(pos == std::string::npos)
+	TSimulatorQualityDist::TSimulatorQualityDist(std::string &s, TRandomGenerator *RandomGenerator) : _randomGenerator(RandomGenerator) {
+	const auto pos1 = s.find("(");
+	if (pos1 == std::string::npos)
 		_max = coretools::str::convertStringCheck<uint8_t>(s);
-	else if(pos == 0){
-		pos = s.find(')');
-		if(pos == std::string::npos || pos != s.size() - 1)
+	else if (pos1 == 0) {
+		const auto pos2 = s.find(')');
+		if (pos2 != s.size() - 1)
 			throw "Failed to understand fixed distribution '" + s + "'!";
-		_max = coretools::str::convertStringCheck<uint8_t>(s.substr(1,pos - 1));
+		_max = coretools::str::convertStringCheck<uint8_t>(s.substr(1, pos2 - 1));
 	} else {
 		throw "Failed to understand fixed distribution '" + s + "'!";
 	}
+}
 
-	_min = 0;
-	_maxPlusOne = -1;
-	_mean = -1.0;
-	_sd = -1.0;
-	_randomGenerator = RandomGenerator;
-};
+void TSimulatorQualityDist::sample(std::vector<PhredIntProbability> &phredInt) const noexcept {
+	for (auto &q : phredInt) q = sample();
+}
 
-TSimulatorQualityDist::TSimulatorQualityDist(TRandomGenerator* RandomGenerator){
-	_max = 30;
-	_min = 0;
-	_maxPlusOne = -1;
-	_mean = -1.0;
-	_sd = -1.0;
-	_randomGenerator = RandomGenerator;
-};
-
-void TSimulatorQualityDist::sample(std::vector<PhredIntProbability> & phredInt) const{
-	for(auto& q : phredInt){
-		q = sample();
-	}
-};
-
-std::string TSimulatorQualityDist::_details() const{
+std::string TSimulatorQualityDist::_details() const {
 	return "fixed quality of " + toString(_max);
-};
+}
 
-void TSimulatorQualityDist::printDetails(coretools::TLog* logfile, const std::string & Name) const{
+void TSimulatorQualityDist::printDetails(coretools::TLog *logfile, const std::string &Name) const {
 	logfile->list(Name + ": " + _details());
-};
+}
 
 //------------------------------------------------
 // TSimulatorQualityDistBinned
 //------------------------------------------------
-TSimulatorQualityDistBinned::TSimulatorQualityDistBinned(std::string & s, TRandomGenerator* RandomGenerator):TSimulatorQualityDist(RandomGenerator){
-	size_t pos = s.find("(");
-	if(pos == 0){
-		s.erase(0,1);
-		pos = s.find(')');
-		if(pos == std::string::npos || pos != s.size() - 1)
+TSimulatorQualityDistBinned::TSimulatorQualityDistBinned(std::string &s, TRandomGenerator *RandomGenerator)
+	: TSimulatorQualityDist(RandomGenerator) {
+	const auto pos1 = s.find("(");
+	if (pos1 == 0) {
+		s.erase(0, 1);
+		const auto pos2 = s.find(')');
+		if (pos2 == std::string::npos || pos2 != s.size() - 1)
 			throw "Failed to understand binned distribution '" + s + "'! Use binned(quality_1,quality_2,..,quality_n).";
-		s.erase(pos,1);
+		s.erase(pos2, 1);
 		coretools::str::fillContainerFromString(s, _qualBins, ',', false, true, false);
 	} else {
 		throw "Failed to understand binned distribution '" + s + "'! Use binned(quality_1,quality_2,..,quality_n).";
 	}
-};
+}
 
-PhredIntProbability TSimulatorQualityDistBinned::sample() const{
-	size_t tmp = _randomGenerator->sample(_qualBins.size());
-	return _qualBins[tmp];
-};
+PhredIntProbability TSimulatorQualityDistBinned::sample() const noexcept {
+	return _qualBins[_randomGenerator->sample(_qualBins.size())];
+}
 
-std::string TSimulatorQualityDistBinned::_details() const{
-	std::string tmpS = coretools::str::concatenateString(_qualBins, ", ");
-	return "uniformly distributed among the values " + tmpS;
-};
+std::string TSimulatorQualityDistBinned::_details() const {
+	return "uniformly distributed among the values " + coretools::str::concatenateString(_qualBins, ", ");
+}
 
 //------------------------------------------------
 // TSimulatorQualityDistFreq
 //------------------------------------------------
-TSimulatorQualityDistFreq::TSimulatorQualityDistFreq(std::string & s, TRandomGenerator* RandomGenerator):TSimulatorQualityDist(RandomGenerator){
-	size_t pos = s.find('(');
-	if(pos == 0){
-		s.erase(0,1);
-		pos = s.find(')');
-		if(pos == std::string::npos || pos != s.size() - 1){
-			throw "Failed to understand frequency distribution '" + s + "'! Use frequency(quality_1:frequency_1,quality_2:frequency_2, ... ,quality_n:frequency_n).";
+TSimulatorQualityDistFreq::TSimulatorQualityDistFreq(std::string &s, TRandomGenerator *RandomGenerator)
+	: TSimulatorQualityDist(RandomGenerator) {
+	const auto pos1 = s.find("(");
+	if (pos1 == 0) {
+		s.erase(0, 1);
+		const auto pos2 = s.find(')');
+		if (pos2 == std::string::npos || pos2 != s.size() - 1) {
+			throw "Failed to understand frequency distribution '" + s +
+				"'! Use frequency(quality_1:frequency_1,quality_2:frequency_2, ... ,quality_n:frequency_n).";
 		}
-		s.erase(pos,1);
+		s.erase(pos2, 1);
 		std::vector<std::string> tmp;
 		coretools::str::fillContainerFromString(s, tmp, ',');
 
-		//now parse each bin
+		// now parse each bin
 		_qualBins.resize(tmp.size());
 		_frequencies.resize(tmp.size());
 
-		for(size_t i = 0; i < tmp.size(); ++i){
-			pos = tmp[i].find(':');
-			if(pos == std::string::npos){
-				throw "Failed to understand frequency distribution '" + s + "'! Use frequency(quality_1:frequency_1,quality_2:frequency_2, ... ,quality_n:frequency_n).";
+		for (size_t i = 0; i < tmp.size(); ++i) {
+			const auto pos3 = tmp[i].find(':');
+			if (pos3 == std::string::npos) {
+				throw "Failed to understand frequency distribution '" + s +
+					"'! Use frequency(quality_1:frequency_1,quality_2:frequency_2, ... ,quality_n:frequency_n).";
 			}
-			_qualBins[i] = tmp[i].substr(0, pos);
-			_frequencies[i] = tmp[i].substr(pos+1);
+			_qualBins[i]    = tmp[i].substr(0, pos3);
+			_frequencies[i] = tmp[i].substr(pos3 + 1);
 		}
 
-		//fill cumulative
+		// fill cumulative
 		coretools::fillCumulative(_frequencies, _cumulativeFrequencies);
 
 	} else {
-		throw "Failed to understand frequency distribution '" + s + "'! Use frequency(quality_1:frequency_1,quality_2:frequency_2, ... ,quality_n:frequency_n).";
+		throw "Failed to understand frequency distribution '" + s +
+			"'! Use frequency(quality_1:frequency_1,quality_2:frequency_2, ... ,quality_n:frequency_n).";
 	}
-};
+}
 
-PhredIntProbability TSimulatorQualityDistFreq::sample() const{
+PhredIntProbability TSimulatorQualityDistFreq::sample() const noexcept {
 	return _qualBins[_randomGenerator->pickOne(_cumulativeFrequencies)];
-};
+}
 
-std::string TSimulatorQualityDistFreq::_details() const{
-	return "frequency bins " + coretools::str::concatenateString(coretools::str::paste(_qualBins, _frequencies, ":"), ", ");
-};
+std::string TSimulatorQualityDistFreq::_details() const {
+	return "frequency bins " +
+	       coretools::str::concatenateString(coretools::str::paste(_qualBins, _frequencies, ":"), ", ");
+}
 
 //------------------------------------------------
 // TSimulatorQualityDistNormal
 //------------------------------------------------
-TSimulatorQualityDistNormal::TSimulatorQualityDistNormal(std::string & s, TRandomGenerator* RandomGenerator):TSimulatorQualityDist(RandomGenerator){
+TSimulatorQualityDistNormal::TSimulatorQualityDistNormal(std::string &s, TRandomGenerator *RandomGenerator)
+	: TSimulatorQualityDist(RandomGenerator) {
 	parseFunctionString(s);
-	_maxPlusOne = _max.get() + 1;
-
 	fillDensities();
-};
+}
 
-TSimulatorQualityDistNormal::TSimulatorQualityDistNormal(double & mean, double & sd, int min, int max, TRandomGenerator* RandomGenerator):TSimulatorQualityDist(RandomGenerator){
+TSimulatorQualityDistNormal::TSimulatorQualityDistNormal(double mean, double sd, int min, int max,
+							 TRandomGenerator *RandomGenerator)
+	: TSimulatorQualityDist(RandomGenerator) {
 	_mean = mean;
-	_sd = sd;
-	_min = min;
-	_max = max;
-	_maxPlusOne = _max.get() + 1;
-
+	_sd   = sd;
+	_min  = min;
+	_max  = max;
 	fillDensities();
-};
+}
 
-void TSimulatorQualityDistNormal::parseFunctionString(std::string & s){
+void TSimulatorQualityDistNormal::parseFunctionString(std::string &s) {
 	std::string orig = s;
 
-	if(s[0] != '(')
-		throw "Fail to understand distribution '" + orig + "': use format normal(mean,sd)[min,max].";
-	s.erase(0,1);
+	if (s[0] != '(') throw "Fail to understand distribution '" + orig + "': use format normal(mean,sd)[min,max].";
+	s.erase(0, 1);
 
 	auto pos = s.find(",");
-	if(pos == std::string::npos)
+	if (pos == std::string::npos)
 		throw "Fail to understand distribution '" + orig + "': use format normal(mean,sd)[min,max].";
-	_mean = coretools::str::convertString<double>(s.substr(0,pos));
-	if(_mean < 0)
-		throw "Fail to understand distribution '" + orig + "': mean must be > 0.";
-	s.erase(0,pos+1);
+	_mean = coretools::str::convertString<double>(s.substr(0, pos));
+	if (_mean < 0) throw "Fail to understand distribution '" + orig + "': mean must be > 0.";
+	s.erase(0, pos + 1);
 
 	pos = s.find(")");
-	if(pos == std::string::npos)
+	if (pos == std::string::npos)
 		throw "Fail to understand distribution '" + orig + "': use format normal(mean,sd)[min,max].";
-	_sd = coretools::str::convertString<double>(s.substr(0,pos));
-	if(_sd < 0)
-			throw "Fail to understand distribution '" + orig + "': sd must be > 0.";
-	s.erase(0,pos+1);
+	_sd = coretools::str::convertString<double>(s.substr(0, pos));
+	if (_sd < 0) throw "Fail to understand distribution '" + orig + "': sd must be > 0.";
+	s.erase(0, pos + 1);
 
-	if(s[0] != '[')
-		throw "Fail to understand distribution '" + orig + "': use format normal(mean,sd)[min,max].";
-	s.erase(0,1);
+	if (s[0] != '[') throw "Fail to understand distribution '" + orig + "': use format normal(mean,sd)[min,max].";
+	s.erase(0, 1);
 	pos = s.find(",");
-	if(pos == std::string::npos)
+	if (pos == std::string::npos)
 		throw "Fail to understand distribution '" + orig + "': use format normal(mean,sd)[min,max].";
-	_min = coretools::str::convertString<PhredIntProbability>(s.substr(0,pos));
-	s.erase(0,pos+1);
+	_min = coretools::str::convertString<PhredIntProbability>(s.substr(0, pos));
+	s.erase(0, pos + 1);
 	pos = s.find("]");
-	if(pos == std::string::npos)
+	if (pos == std::string::npos)
 		throw "Fail to understand distribution '" + orig + "': use format normal(mean,sd)[min,max].";
-	_max = coretools::str::convertString<PhredIntProbability>(s.substr(0,pos));
-	if(_max < _min)
-			throw "Fail to understand distribution '" + orig + "': max must be >= min!";
-};
+	_max = coretools::str::convertString<PhredIntProbability>(s.substr(0, pos));
+	if (_max < _min) throw "Fail to understand distribution '" + orig + "': max must be >= min!";
+}
 
-void TSimulatorQualityDistNormal::fillDensities(){
-	//fill densities
-	_size = _maxPlusOne.get() - _min.get();
-	_densities.resize(_size);
-	_cumulDensities.resize(_size);
+void TSimulatorQualityDistNormal::fillDensities() {
+	using coretools::TNormalDistr;
+	// fill densities
+	const auto size = _max.get() + 1 - _min.get();
+	_densities.resize(size);
+	_cumulDensities.resize(size);
 
-	double nextDens = coretools::TNormalDistr::cumulativeDistrFunction(_min.get() - 0.5, _mean, _sd*_sd);
+	double nextDens = TNormalDistr::cumulativeDistrFunction(_min.get() - 0.5, _mean, _sd * _sd);
 	double prevDens;
 	double sum = 0;
-	for(int i=0; i<_size; ++i){
-		prevDens = nextDens;
-		nextDens = coretools::TNormalDistr::cumulativeDistrFunction(_min.get() + i + 0.5, _mean, _sd*_sd);
-		_densities[i] =  nextDens - prevDens;
+	for (int i = 0; i < size; ++i) {
+		prevDens      = nextDens;
+		nextDens      = TNormalDistr::cumulativeDistrFunction(_min.get() + i + 0.5, _mean, _sd * _sd);
+		_densities[i] = nextDens - prevDens;
 		sum += _densities[i];
 	}
 
-	//normalize
-	for(int i=0; i<_size; ++i)
-		_densities[i] /= sum;
+	// normalize
+	for (int i = 0; i < size; ++i) _densities[i] /= sum;
 
-	//fill cumulative
+	// fill cumulative
 	_cumulDensities[0] = _densities[0];
-	for(int i=1; i<_size; ++i)
-		_cumulDensities[i] = _cumulDensities[i-1] + _densities[i];
-	_cumulDensities[_size-1] = 1.0;
+	for (int i = 1; i < size; ++i) _cumulDensities[i] = _cumulDensities[i - 1] + _densities[i];
+	_cumulDensities[size - 1] = 1.0;
 }
 
-PhredIntProbability TSimulatorQualityDistNormal::sample() const {
-	return PhredIntProbability( _randomGenerator->pickOne(_cumulDensities) + _min.get() );
-};
+PhredIntProbability TSimulatorQualityDistNormal::sample() const noexcept {
+	return PhredIntProbability(_randomGenerator->pickOne(_cumulDensities) + _min.get());
+}
 
-std::string TSimulatorQualityDistNormal::_details() const{
-	return coretools::str::toString("Normally distributed quality scores with mean=", _mean, " and sd=", _sd, ", truncated to [", _min, ",", _max, "].");
-};
+std::string TSimulatorQualityDistNormal::_details() const {
+	return coretools::str::toString("Normally distributed quality scores with mean=", _mean, " and sd=", _sd,
+	                                ", truncated to [", _min, ",", _max, "].");
+}
 
-}; //end namesapce
-
+} // namespace Simulations
--- Simulations/TSimulatorQuality.h
+++ Simulations/TSimulatorQuality.h
@@ -10,14 +10,14 @@
 
 #include "../BAM/TSequencedBase.h"
 #include "TLog.h"
-#include "TRandomGenerator.h"
 #include "TParameters.h"
-#include "TSimulatorReadLength.h"
+#include "TRandomGenerator.h"
 #include "TSequencingErrorModel.h"
-#include "mathFunctions.h"
+#include "TSimulatorReadLength.h"
 #include "algorithmsAndVectors.h"
+#include "mathFunctions.h"
 
-namespace Simulations{
+namespace Simulations {
 
 using coretools::TRandomGenerator;
 using genometools::PhredIntProbability;
@@ -26,85 +26,78 @@ using genometools::PhredIntProbability;
 // TSimulatorQualityDist
 // Used for quality and mapping quality
 //-------------------------------
-//Class of a fixed value
-class TSimulatorQualityDist{
+// Class of a fixed value
+class TSimulatorQualityDist {
 protected:
-	TRandomGenerator* _randomGenerator;
-	PhredIntProbability _min;
-	PhredIntProbability _max;
-	PhredIntProbability _maxPlusOne;
-	double _mean, _sd;
+	TRandomGenerator *_randomGenerator;
+	PhredIntProbability _min{0};
+	PhredIntProbability _max{30};
+	double _mean = -1.;
+	double _sd   = -1.;
 
 	virtual std::string _details() const;
-
 public:
-	TSimulatorQualityDist(TRandomGenerator* RandomGenerator);
-	TSimulatorQualityDist(std::string & s, TRandomGenerator* RandomGenerator);
+	TSimulatorQualityDist(TRandomGenerator *RandomGenerator) : _randomGenerator(RandomGenerator){};
+	TSimulatorQualityDist(std::string &s, TRandomGenerator *RandomGenerator);
 	virtual ~TSimulatorQualityDist(){};
-	PhredIntProbability min(){ return _min; };
-	PhredIntProbability max(){ return _max; };
-	double mean(){return _mean; };
-	double sd(){return _sd; };
+	PhredIntProbability min() const noexcept { return _min; };
+	PhredIntProbability max() const noexcept { return _max; };
+	double mean() const noexcept { return _mean; };
+	double sd() const noexcept { return _sd; };
 
-	virtual PhredIntProbability sample() const { return _max; };
-	virtual void sample(std::vector<PhredIntProbability> & phredInt) const;
-	void printDetails(coretools::TLog* logfile, const std::string & Name) const;
+	virtual PhredIntProbability sample() const noexcept { return _max; };
+	virtual void sample(std::vector<PhredIntProbability> &phredInt) const noexcept;
+	void printDetails(coretools::TLog *logfile, const std::string &Name) const;
 };
 
 //------------------------------------------------
 // TSimulatorQualityDistBinned
 // Class of a binned distribution
 //------------------------------------------------
-class TSimulatorQualityDistBinned:public TSimulatorQualityDist{
+class TSimulatorQualityDistBinned : public TSimulatorQualityDist {
 private:
 	std::vector<PhredIntProbability> _qualBins;
-
 	std::string _details() const override;
-
 public:
-	TSimulatorQualityDistBinned(std::string & s, TRandomGenerator* RandomGenerator);
-	PhredIntProbability sample() const override;
+	TSimulatorQualityDistBinned(std::string &s, TRandomGenerator *RandomGenerator);
+	PhredIntProbability sample() const noexcept override;
 };
 
 //------------------------------------------------
 // TSimulatorQualityDistFreq
 // Class of a binned distribution with frequencies
 //------------------------------------------------
-class TSimulatorQualityDistFreq:public TSimulatorQualityDist{
+class TSimulatorQualityDistFreq : public TSimulatorQualityDist {
 private:
 	std::vector<PhredIntProbability> _qualBins;
 	std::vector<coretools::Probability> _frequencies;
 	std::vector<coretools::Probability> _cumulativeFrequencies;
 
 	std::string _details() const override;
-
 public:
-	TSimulatorQualityDistFreq(std::string & s, TRandomGenerator* RandomGenerator);
-	PhredIntProbability sample() const override;
+	TSimulatorQualityDistFreq(std::string &s, TRandomGenerator *RandomGenerator);
+	PhredIntProbability sample() const noexcept override;
 };
 
 //------------------------------------------------
 // TSimulatorQualityDistNormal
 // Class of a normal distribution
 //------------------------------------------------
-class TSimulatorQualityDistNormal:public TSimulatorQualityDist{
+class TSimulatorQualityDistNormal : public TSimulatorQualityDist {
 private:
-	//densities
-	int _size;
+	// densities
 	std::vector<double> _densities;
 	std::vector<double> _cumulDensities;
 
 	std::string _details() const override;
-
 public:
-	TSimulatorQualityDistNormal(std::string & s, TRandomGenerator* RandomGenerator);
-	TSimulatorQualityDistNormal(double & mean, double & sd, int min, int max, TRandomGenerator* RandomGenerator);;
-	void parseFunctionString(std::string & s);
+	TSimulatorQualityDistNormal(std::string &s, TRandomGenerator *RandomGenerator);
+	TSimulatorQualityDistNormal(double mean, double sd, int min, int max, TRandomGenerator *RandomGenerator);
+	void parseFunctionString(std::string &s);
 	void fillDensities();
-	PhredIntProbability sample() const override;
+	PhredIntProbability sample() const noexcept override;
 };
 
-}; //end namespace
-
+} // namespace Simulations
 
 #endif /* SIMULATIONS_TSIMULATORQUALITY_H_ */
