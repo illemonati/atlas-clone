5f1a6731e3110abcd7b21ceb68f9d70ad35624d2
Madleina Caduff <madleina.caduff@gmx.ch>
2022-06-09 11:49:24 +0200

    fixed example convertVCF.sh; updated to newest genometools

--- examples/convertVCF.sh
+++ examples/convertVCF.sh
@@ -9,6 +9,6 @@ $atlas --task simulate --vcf --type HW --sampleSize 10 --chrLength 100000 --fixe
 $atlas --task convertVCF --vcf ATLAS_simulations.vcf.gz --format beagle
 $atlas --task convertVCF --vcf ATLAS_simulations.vcf.gz --format geno
 $atlas --task convertVCF --vcf ATLAS_simulations.vcf.gz --format LFMM --genotypes call
-$atlas --task convertVCF --vcf ATLAS_simulations.vcf.gz --format LFMM --genotypes posterior --minSamplesWithData 10
+$atlas --task convertVCF --vcf ATLAS_simulations.vcf.gz --format LFMM --genotypes posterior --maxMissing 0.0
 $atlas --task convertVCF --vcf ATLAS_simulations.vcf.gz --format posfile
 $atlas --task convertVCF --vcf ATLAS_simulations.vcf.gz --format genfile
