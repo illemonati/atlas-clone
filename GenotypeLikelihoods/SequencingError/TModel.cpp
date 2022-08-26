4b6c1dd30e2adda4eaf1f35a2449553b9ff924f9
Andreas Füglistaler <andreas.fueglistaler@unifr.ch>
2022-08-26 08:53:02 +0200

    correcting rho output

--- GenotypeLikelihoods/SequencingError/TModel.cpp
+++ GenotypeLikelihoods/SequencingError/TModel.cpp
@@ -73,7 +73,7 @@ TRho::TRho(const std::string &Def) {
 std::string TRho::getDefinition() const noexcept {
 	using coretools::str::toString;
 	return "[[-,"s + toString(_rho[Base::A][Base::C]) + ',' + toString(_rho[Base::A][Base::G]) + ',' + toString(_rho[Base::A][Base::T]) + "]["
-		+ toString(_rho[Base::C][Base::A]) + ",-," + toString(_rho[Base::C][Base::G]) + toString(_rho[Base::C][Base::T]) + "]["
+		+ toString(_rho[Base::C][Base::A]) + ",-," + toString(_rho[Base::C][Base::G]) + ',' + toString(_rho[Base::C][Base::T]) + "]["
 		+ toString(_rho[Base::G][Base::A]) + ',' + toString(_rho[Base::G][Base::C]) + ",-," + toString(_rho[Base::G][Base::T]) + "]["
 		+ toString(_rho[Base::T][Base::A]) + ',' + toString(_rho[Base::T][Base::C]) + ',' + toString(_rho[Base::T][Base::G]) + ",-]]";
 }
