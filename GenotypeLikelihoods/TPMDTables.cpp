9c69895f55a38667a1d1e7369b31f7d86980177c
Andreas Füglistaler <andreas.fueglistaler@unifr.ch>
2022-08-26 09:44:48 +0200

    fixing pooled pmd for pooled readgroups

--- GenotypeLikelihoods/TPMDTables.cpp
+++ GenotypeLikelihoods/TPMDTables.cpp
@@ -81,24 +81,27 @@ void TPMDTables::initialize(const BAM::TReadGroups *ReadGroups, size_t TableLeng
 	_readGroups   = ReadGroups;
 	_readGroupMap = ReadGroupMap;
 	_tableLength  = TableLength;
-	_tables.resize(_readGroupMap->numReadGroupsInUse());
-	for (auto &ts : _tables)
-		for (auto &t : ts) t.resize(TableLength);
+	//_tables.resize(_readGroupMap->numReadGroupsInUse()); // ReadgroupMap not continuous
+	_tables.resize(_readGroups->size());
+	for (auto i = _readGroups->cbegin(); i != _readGroups->cend(); ++i) {
+		auto & table = _tables[_readGroupMap->pooledIndex(i->id())];
+		for (auto &t : table) t.resize(TableLength);
+	}
 }
 
 void TPMDTables::add(const BAM::TSequencedBase &base, genometools::Base reference) {
 	const auto from3 = base.distFrom3Prime < base.distFrom5Prime;
-	const auto i_rg  = _readGroupMap->pooledIndex(base.readGroupID);
+	auto & table = _tables[_readGroupMap->pooledIndex(base.readGroupID)];
 	if (base.isReverseStrand()) {
 		if (from3)
-			_tables[i_rg][reverse3].add(base.distFrom3Prime, flipped(reference), flipped(base.base));
+			table[reverse3].add(base.distFrom3Prime, flipped(reference), flipped(base.base));
 		else
-			_tables[i_rg][reverse5].add(base.distFrom5Prime, flipped(reference), flipped(base.base));
+			table[reverse5].add(base.distFrom5Prime, flipped(reference), flipped(base.base));
 	} else {
 		if (from3)
-			_tables[i_rg][forward3].add(base.distFrom3Prime, reference, base.base);
+			table[forward3].add(base.distFrom3Prime, reference, base.base);
 		else
-			_tables[i_rg][forward5].add(base.distFrom5Prime, reference, base.base);
+			table[forward5].add(base.distFrom5Prime, reference, base.base);
 	}
 }
 
@@ -116,12 +119,12 @@ void TPMDTables::write(std::string filename, bool normalize) {
 	std::vector<std::string> prefix(4);
 	for (int i = 0; i < _readGroups->size(); ++i) {
 		if (_readGroups->readGroupInUse(i)) {
-			size_t i_rg = _readGroupMap->pooledIndex(i);
+			auto & table = _tables[_readGroupMap->pooledIndex(i)];
 			prefix[0]   = _readGroups->getName(i);
 			for (size_t j = 0; j < 4; ++j) {
 				prefix[1] = prefix1[j];
 				prefix[2] = prefix2[j];
-				_tables[i_rg][j].write(out, prefix, normalize);
+				table[j].write(out, prefix, normalize);
 			}
 		}
 	}
