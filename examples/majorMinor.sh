3b7d849f7852028c6ff877d22092c33ad87385e7
Andreas Füglistaler <andreas.fueglistaler@unifr.ch>
2022-09-06 14:40:48 +0200

    improving find_atlas, better majorMinor test

--- examples/majorMinor.sh
+++ examples/majorMinor.sh
@@ -2,14 +2,14 @@
 
 . $(dirname $0)/find_atlas
 
-N=20
+N=100
 if [[ "$#" -eq 1 ]]; then
     N=$1
 fi
 
 echo "doing $N samples"
 
-$atlas --task simulate --type HW --F 0.1 --chrLength 1000 --sampleSize $N --fracPoly 1.0 --alpha 2.0 --beta 2.0 --fixedSeed 0 --logFile simulate.out
+$atlas --task simulate --type HW --F 0.1 --chrLength 10000 --sampleSize $N --fracPoly 0.1 --alpha 2.0 --beta 2.0 --fixedSeed 0 --logFile simulate.out
 
 for f in *.bam; do
     $atlas --task GLF --bam $f --fasta ATLAS_simulations.fasta --fixedSeed 0 --logFile GLF_${f%.bam}.out
