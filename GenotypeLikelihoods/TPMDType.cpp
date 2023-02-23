bb038615f36db06a8ce360e9fce2e238164f2093
Andreas <andreas.fueglistaler@protonmail.com>
2023-02-23 17:30:01 +0100

    avoid repetition

--- GenotypeLikelihoods/TPMDType.cpp
+++ GenotypeLikelihoods/TPMDType.cpp
@@ -1,14 +1,53 @@
 #include "TPMDType.h"
 #include "coretools/Main/TLog.h"
 #include "coretools/Main/TRandomGenerator.h"
+#include "coretools/Types/probability.h"
 
 namespace GenotypeLikelihoods {
 
 using coretools::instances::logfile;
 using coretools::instances::randomGenerator;
 using genometools::Base;
-
 using namespace coretools::str;
+
+namespace impl {
+TBaseLikelihoods baseLikelihoods(coretools::Probability pCT, coretools::Probability pGA,
+								 const TBaseLikelihoods &baseLikelihoodsNoPMD) {
+	TBaseLikelihoods baseLikelihoods(baseLikelihoodsNoPMD);
+	baseLikelihoods[Base::C] =
+		(1.0 - pCT) * baseLikelihoodsNoPMD[Base::C] + pCT * baseLikelihoodsNoPMD[Base::T];
+	baseLikelihoods[Base::G] =
+		(1.0 - pGA) * baseLikelihoodsNoPMD[Base::G] + pGA * baseLikelihoodsNoPMD[Base::A];
+	return baseLikelihoods;
+}
+
+TBaseProbabilities massFunction(Base b, coretools::Probability pCT, coretools::Probability pGA,
+								const TBaseLikelihoods &baseLikelihoodsNoPMD) {
+	switch (b) {
+	case Base::A: return TBaseProbabilities::normalize({1., 0., 0., 0.});
+	case Base::C: {
+		return TBaseProbabilities::normalize(
+			{0., (1. - pCT) * baseLikelihoodsNoPMD[Base::C], 0., pCT * baseLikelihoodsNoPMD[Base::T]});
+	}
+	case Base::G: {
+		return TBaseProbabilities::normalize(
+			{pGA * baseLikelihoodsNoPMD[Base::A], 0., (1. - pGA) * baseLikelihoodsNoPMD[Base::G], 0.});
+	}
+	default: return TBaseProbabilities::normalize({0., 0., 0., 1.}); // case Base::T
+	}
+}
+Base simulate(Base base, coretools::Probability pCT, coretools::Probability pGA) {
+	if (base == Base::C) {
+		if (randomGenerator().getRand() < pCT) return Base::T;
+	}
+	else if (base == Base::G) {
+		if (randomGenerator().getRand() < pGA) return Base::A;
+	}
+	// else
+	return base;
+}
+}
+
 TPMDTypeDoubleStrand::TPMDTypeDoubleStrand(const std::vector<std::string> &Details) {
 	// expect three elements: type, pmdCT, pmdGA
 	constexpr size_t nDetails = 3;
@@ -44,46 +83,19 @@ void TPMDTypeDoubleStrand::estimate(const PMDTable_RG &PMDTable,
 	logfile().endIndent();
 }
 
+
 TBaseLikelihoods TPMDTypeDoubleStrand::baseLikelihoods(const BAM::TSequencedBase &data,
 					       const TBaseLikelihoods &baseLikelihoodsNoPMD) const {
-	TBaseLikelihoods baseLikelihoods(baseLikelihoodsNoPMD);
-
-	// get relevant PMD probabilities
-	const auto pmdProb_CT = _probCT(data);
-	const auto pmdProb_GA = _probGA(data);
-
-	// add PMD
-	baseLikelihoods[Base::C] =
-		(1.0 - pmdProb_CT) * baseLikelihoodsNoPMD[Base::C] + pmdProb_CT * baseLikelihoodsNoPMD[Base::T];
-	baseLikelihoods[Base::G] =
-		(1.0 - pmdProb_GA) * baseLikelihoodsNoPMD[Base::G] + pmdProb_GA * baseLikelihoodsNoPMD[Base::A];
-	return baseLikelihoods;
+	return impl::baseLikelihoods(_probCT(data), _probGA(data), baseLikelihoodsNoPMD);
 }
 
 TBaseProbabilities TPMDTypeDoubleStrand::massFunction(Base b, const BAM::TSequencedBase &data,
 														 const TBaseLikelihoods &baseLikelihoodsNoPMD) const {
-	switch (b) {
-	case Base::A: return TBaseProbabilities::normalize({1., 0., 0., 0.});
-	case Base::C: {
-		const auto pmdProb_CT = _probCT(data);
-		return TBaseProbabilities::normalize(
-			{0., (1. - pmdProb_CT) * baseLikelihoodsNoPMD[Base::C], 0., pmdProb_CT * baseLikelihoodsNoPMD[Base::T]});
-	}
-	case Base::G: {
-		const auto pmdProb_GA = _probGA(data);
-		return TBaseProbabilities::normalize(
-			{pmdProb_GA * baseLikelihoodsNoPMD[Base::A], 0., (1. - pmdProb_GA) * baseLikelihoodsNoPMD[Base::G], 0.});
-	}
-	default: return TBaseProbabilities::normalize({0., 0., 0., 1.}); // case Base::T
-	}
+	return impl::massFunction(b, _probCT(data), _probGA(data), baseLikelihoodsNoPMD);
 }
 
 void TPMDTypeDoubleStrand::simulate(BAM::TSequencedBase &data) const {
-	if (data.base == Base::C) {
-		if (randomGenerator().getRand() < _probCT(data)) data.base = Base::T;
-	} else if (data.base == Base::G) {
-		if (randomGenerator().getRand() < _probGA(data)) data.base = Base::A;
-	}
+	data.base = impl::simulate(data.base, _probCT(data), _probGA(data));
 }
 //------------------------------------------------------
 // TPMDSingleStrand
@@ -130,44 +142,16 @@ void TPMDTypeSingleStrand::estimate(const PMDTable_RG &PMDTable,
 
 TBaseLikelihoods TPMDTypeSingleStrand::baseLikelihoods(const BAM::TSequencedBase &data,
 					       const TBaseLikelihoods &baseLikelihoodsNoPMD) const {
-	TBaseLikelihoods baseLikelihoods(baseLikelihoodsNoPMD);
-
-	// get relevant PMD probabilities
-	const auto pmdProb_CT = _probCT(data);
-	const auto pmdProb_GA = _probGA(data);
-
-	// add PMD
-	baseLikelihoods[Base::C] =
-		(1.0 - pmdProb_CT) * baseLikelihoodsNoPMD[Base::C] + pmdProb_CT * baseLikelihoodsNoPMD[Base::T];
-	baseLikelihoods[Base::G] =
-		(1.0 - pmdProb_GA) * baseLikelihoodsNoPMD[Base::G] + pmdProb_GA * baseLikelihoodsNoPMD[Base::A];
-	return baseLikelihoods;
+	return impl::baseLikelihoods(_probCT(data), _probGA(data), baseLikelihoodsNoPMD);
 }
 
 TBaseProbabilities TPMDTypeSingleStrand::massFunction(Base b, const BAM::TSequencedBase &data,
 														 const TBaseLikelihoods &baseLikelihoodsNoPMD) const {
-	switch (b) {
-	case Base::A: return TBaseProbabilities::normalize({1., 0., 0., 0.});
-	case Base::C: {
-		const auto pmdProb_CT = _probCT(data);
-		return TBaseProbabilities::normalize(
-			{0., (1. - pmdProb_CT) * baseLikelihoodsNoPMD[Base::C], 0., pmdProb_CT * baseLikelihoodsNoPMD[Base::T]});
-	}
-	case Base::G: {
-		const auto pmdProb_GA = _probGA(data);
-		return TBaseProbabilities::normalize(
-			{pmdProb_GA * baseLikelihoodsNoPMD[Base::A], 0., (1. - pmdProb_GA) * baseLikelihoodsNoPMD[Base::G], 0.});
-	}
-	default: return TBaseProbabilities::normalize({0., 0., 0., 1.}); // case Base::T
-	}
+	return impl::massFunction(b, _probCT(data), _probGA(data), baseLikelihoodsNoPMD);
 }
 
 void TPMDTypeSingleStrand::simulate(BAM::TSequencedBase &data) const {
-	if (data.base == Base::C) {
-		if (randomGenerator().getRand() < _probCT(data)) data.base = Base::T;
-	} else if (data.base == Base::G) {
-		if (randomGenerator().getRand() < _probGA(data)) data.base = Base::A;
-	}
+	data.base = impl::simulate(data.base, _probCT(data), _probGA(data));
 }
 
 TPMDType *makeType(std::string_view pmdString) {
