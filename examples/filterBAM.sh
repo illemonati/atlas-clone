7dea34499a29e4ca9888300ce2e73985323f6fab
Andreas <andreas.fueglistaler@protonmail.com>
2023-02-27 15:10:22 +0100

    fixing softClip simulator initialization

--- examples/filterBAM.sh
+++ examples/filterBAM.sh
@@ -7,6 +7,6 @@ $atlas --task simulate --type HW --F 0.1 --sampleSize 20 --chrLength 1000 --frac
 for i in {1..20}; do
 samtools view ATLAS_simulations_ind$i.bam | head -250 | tail -10 | cut -f1 > blacklist_$i.txt
 u=$(echo "$i*5" | bc)
-$atlas --task filterBAM --bam ATLAS_simulations_ind$i.bam --filterSoftClips --fixedSeed 0 --filterMQ 0,$u --blacklist blacklist_$i.txt --filterReadLength 0,$u --filterFragmentLength 0,$u --logFile filterBAM_$i.out
+$atlas --task filterBAM --bam ATLAS_simulations_ind$i.bam --fixedSeed 0 --filterMQ 0,$u --blacklist blacklist_$i.txt --filterReadLength 0,$u --filterFragmentLength 0,$u --logFile filterBAM_$i.out
 done
 
