b601aa1c62f61be4c1f150b31d4bcaf75242afd1
Andreas Füglistaler <andreas.fueglistaler@unifr.ch>
2023-04-21 15:59:04 +0200

    minimum chrlength for gzstream not to complain

--- examples/VCFCompare.sh
+++ examples/VCFCompare.sh
@@ -2,8 +2,8 @@
 
 . $(dirname $0)/find_atlas
 
-$atlas --task simulate --vcf --type HW --sampleSize 2 --chrLength 1000 --ploidy 2 --fixedSeed 1 --out simulate1 --logFile simulate1.out
-$atlas --task simulate --vcf --type HW --sampleSize 2 --chrLength 1000 --ploidy 2 --fixedSeed 2 --out simulate2 --logFile simulate2.out
+$atlas --task simulate --vcf --type HW --sampleSize 2 --chrLength 2000 --ploidy 2 --fixedSeed 1 --out simulate1 --logFile simulate1.out
+$atlas --task simulate --vcf --type HW --sampleSize 2 --chrLength 2000 --ploidy 2 --fixedSeed 2 --out simulate2 --logFile simulate2.out
 
 $atlas --task VCFCompare --vcf simulate1.vcf.gz --samples ind_0,ind_1 --out VCFCompare_f_ss --logFile VCFCompare_f_ss.out
 $atlas --task VCFCompare --vcf simulate1.vcf.gz,simulate2.vcf.gz --samples ind_0 --out VCFCompare_ff_s --logFile VCFCompare_ff_s.out
--- examples/VCFDiagnostics.sh
+++ examples/VCFDiagnostics.sh
@@ -2,7 +2,7 @@
 
 . $(dirname $0)/find_atlas
 
-$atlas --task simulate --vcf --type HW --sampleSize 2 --chrLength 1000 --ploidy 2 --fixedSeed 1 --out simulate --logFile simulate.out
+$atlas --task simulate --vcf --type HW --sampleSize 2 --chrLength 2000 --ploidy 2 --fixedSeed 1 --out simulate --logFile simulate.out
 
 $atlas --task VCFDiagnostics --vcf simulate.vcf.gz --out VCFDiagnostics --logFile VCFDiagnostics.out 
 $atlas --task VCFDiagnostics --fixInt --vcf simulate.vcf.gz --out VCFFixInt --logFile VCFFixInt.out
