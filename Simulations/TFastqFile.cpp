7e9b5bf516ee0e0fb078083b5977d2998d9e7b26
Michael Jopiti <michael@wifi-unifr-144-212.unifr.ch>
2023-05-03 15:02:49 +0200

    fastq Simulation for single individual is working properly

--- Simulations/TFastqFile.cpp
+++ Simulations/TFastqFile.cpp
@@ -23,53 +23,26 @@
 #include "coretools/Files/TWriter.h"
 #include "coretools/Files/TReader.h"
 #include "coretools/Files/TFile.h"
+#include "coretools/Strings/toString.h"
 #include "coretools/Strings/stringFunctions.h" // for toString, readAfterLast, stringStartsWith
 #include "TSimulatedOutputFile.h"
+
+#include <iostream>
 
 using coretools::instances::logfile; 	//used to write log file
 
 namespace FASTQ{
 
-
-//------------------------------------------------
-// Methods to operate on Fastq files
-//------------------------------------------------
-
     TFastqFile::TFastqFile(std::string_view fileName){
         _fileName = fileName;
         static coretools::TOutputFile txtFile(fileName);
+        _ptrFile = &txtFile;
     }
 
-
-//------------------------------------------------
-// Private methods
-//------------------------------------------------
-
-    void TFastqFile::_writeAlignment(const BAM::TAlignment &alignment){
-        //alignment.flags
-        //alignment.isEmpty
-        //alignment.readGroupId
-        //uint16_t readGroupId() const { return _readGroupID; };
-        //alignment.refID
-        //constexpr size_t refID() const noexcept { return _refID; };
-        //alignment.qualities
-        //std::vector<uint32_t> _qualities;
-        //alignment.binQualityScoresIllumina
-        //alignment.length
-        //alignment.isPaired
-        //alignment.mateRefID
-        //alignment.clear
-        //alignment.end
-
-
-//takes alignment sequence and qualities and writes it in the file
-
-    }
 
 //------------------------------------------------
 // Public methods
 //------------------------------------------------
-
 
     void TFastqFile::open(std::string_view filename){
         //implement txtFile open()
@@ -81,17 +54,13 @@ namespace FASTQ{
     }
 
     void TFastqFile::writeAlignment(const BAM::TAlignment &alignment){
-
-        _writeAlignment(alignment);
+    //takes alignment sequence and qualities and writes it in the file
+        _ptrFile->writeln("@" + std::to_string(alignment.readGroupId())
+                            + ":" + std::to_string(alignment.refID()));
+        _ptrFile->writeln(alignment.sequence());
+        _ptrFile->writeln(alignment.qualities());
+        _ptrFile->writeln("+");
     }
-
-    void writeAlignmentLater(const BAM::TAlignment &alignment){
-        //just to test if this method is the problem to lack of vtable
-    }
-
-    void TFastqFile::setHeader(std::string header){  _header = header; }
-
-    void TFastqFile::setHeader(){}
 
     std::string_view TFastqFile::getName() {
         return _fileName;
--- Simulations/TFastqFile.h
+++ Simulations/TFastqFile.h
@@ -33,52 +33,20 @@ namespace Simulations { class TSimulatedOutputFile; }
 namespace FASTQ{
 
     class TFastqFile : public Simulations::TSimulatedOutputFile {
-        //create temporary file from public coretools::TFile, not good practice to do multiple inheritance
 
     private:
-        //istances regarding file handling
         std::string _fileName;
         std::string_view outputName;
-        //coretools::TOutputFile txtFile;
-
-        //istances regarding file content (mainly for indexed file)
-        bool _open = false;
-        std::string _header;
-        std::string _tmpRefID;
-        uint16_t _readGroupID;
-
-        //istances regarding writing Fastq information such as sequence, Phred quality score and queues for sorting alignments per group read
-        BAM::TAlignment *alignment;
-        std::string _sequence;
-        std::string _qualities;
-
-        //queue of pointers to queues of next-sorted alignments and related methods
-        //static std::queue<std::queue<BAM::TAlignment>*> alignmentQueues;
-
-        //we get alignment per alignment, no need for a queue?
-
-        //methods operating on alignment content
-        void _writeAlignment(const BAM::TAlignment &alignment);
+        coretools::TOutputFile* _ptrFile;
 
     public:
         //constructor
         TFastqFile(std::string_view fileName);
 
-        // File object
-        //coretools::TOutputFile _file(fileName);
-
         //methods
         void open(std::string_view filename);
         void close();
         void writeAlignment(const BAM::TAlignment &alignment);
-        void writeAlignmentLater(const BAM::TAlignment &alignment);
-
-        //getters and setters
-        void setHeader(std::string header);     //important to set Header because it is not inherited from TFile since it is not a table
-        void setHeader();            //set with generic identifiers
-        void setOutputName(std::string outputName);
-
-        bool isOpen() const { return _open; };
 
         std::string_view getName();
 
