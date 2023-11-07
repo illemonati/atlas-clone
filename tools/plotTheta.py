dd5965061389556c5d092b0d054a3a16b1475fa6
Andreas <andreas.fueglistaler@protonmail.com>
2023-11-07 09:52:59 +0100

    better plotting

--- tools/plotTheta.py
+++ tools/plotTheta.py
@@ -12,13 +12,14 @@ if __name__ == "__main__":
     parser.add_argument("files", nargs='*', help="Data")
     parser.add_argument("--title", default="theta")
     parser.add_argument("--dMin", type=float, default=0.)
-    parser.add_argument("--vs", default="")
     parser.add_argument("--median",  action="store_true")
     parser.add_argument("--yMin",  type=float, default=1e-4)
     parser.add_argument("--yMax",  type=float, default=1e-2)
+    parser.add_argument("--nPlots",  type=int, default=1)
 
     args = parser.parse_args()
     # get header and cols
+    nFiles = len(args.files)
     for i, file in enumerate(args.files):
         label = file.split("_theta.txt.gz")[0]
         f      = gzip.open(file)
@@ -59,54 +60,44 @@ if __name__ == "__main__":
         fmts= ["o-", "s-", "X-", "d-", "p-", "<-", "^-", ">-"]
         mks = [10, 9, 8, 7, 6, 5, 4, 3, 2]
 
-        if not args.vs:
-            for j in range(len(thetas)):
-                iis = where(u95s[j] < 10.)
-                thetas[j] = thetas[j][iis]
-                print(j, len(l95s[j]), len(thetas[j]))
+        for j in range(len(thetas)):
+            iis = where(u95s[j] < 10.)
+            thetas[j] = thetas[j][iis]
+            print(j, len(l95s[j]), len(thetas[j]))
 
-            if args.median:
-                print("using median")
-                mdepths = [nanmedian(d) for d in depths]
-                mthetas = [nanmedian(t) for t in thetas]
-            else:
-                print("using mean")
-                mdepths = [nanmean(d) for d in depths]
-                mthetas = [nanmean(t) for t in thetas]
-
-            sdepths = [nanstd(d) for d in depths]
-            sthetas = [nanstd(t) for t in thetas]
-
-            print(label)
-            print("depth:", mdepths)
-            print("theta:", mthetas)
-            plt.errorbar(mdepths, mthetas, xerr=sdepths, yerr=sthetas, fmt=fmts[i], markersize=mks[i],linewidth=2, capsize=6, label=label)
-            plt.hlines(mthetas[0], 0, 1.5*max(mdepths), plt.gca().lines[-1].get_color(), "dashed")
-            print(plt.gca().lines[-1].get_color())
-            if i == 0:
-                plt.xscale("log")
-                plt.yscale("log")
-                plt.xlabel(r"Depth")
-                plt.ylabel(r"$\theta$")
-                plt.xlim(max(mdepths)*1.5, min(mdepths)/1.5)
-                plt.xticks(mdepths, ["%2.2f"%(d) for d in mdepths])
+        if args.median:
+            print("using median")
+            mdepths = [nanmedian(d) for d in depths]
+            mthetas = [nanmedian(t) for t in thetas]
         else:
-            s = args.vs.split(",")
-            i1 = int(s[0])
-            m  = 1.1*max(thetas[i1])
-            depth1 = nanmean(depths[i1])
-            plt.plot([0,m],[0,m], "-")
-            for si in s[1:]:
-                i2 = int(si)
-                depth2 = nanmean(depths[i2])
-                plt.plot(thetas[i1], thetas[i2], ".", label="depth = %0.2f"%(depth2))
-            plt.xlabel(r"$\theta$(depth = %0.2f)"%(depth1))
-            plt.ylabel(r"$\theta$(downsample)")
-            plt.gca().set_aspect("equal")
+            print("using mean")
+            mdepths = [nanmean(d) for d in depths]
+            mthetas = [nanmean(t) for t in thetas]
 
-    plt.ylim(args.yMin, args.yMax)
+        sdepths = [nanstd(d) for d in depths]
+        sthetas = [nanstd(t) for t in thetas]
 
-    plt.legend()
-    plt.title(args.title)
+        print(label)
+        print("depth:", mdepths)
+        print("theta:", mthetas)
+
+        filesPerPlots = nFiles/args.nPlots
+        iPlot = int(i/filesPerPlots) + 1
+        plt.subplot(args.nPlots, 1, iPlot)
+
+        plt.errorbar(mdepths, mthetas, xerr=sdepths, yerr=sthetas, fmt=fmts[i], markersize=mks[i],linewidth=2, capsize=6, label=label)
+        plt.hlines(mthetas[0], 0, 1.5*max(mdepths), plt.gca().lines[-1].get_color(), "dashed")
+        print(plt.gca().lines[-1].get_color())
+        if iPlot < int((i+1)/filesPerPlots) + 1:
+            plt.xscale("log")
+            plt.yscale("log")
+            plt.ylabel(r"$\theta$")
+            if iPlot == args.nPlots:
+                plt.xlabel(r"Depth")
+            plt.xlim(max(mdepths)*1.5, min(mdepths)/1.5)
+            plt.xticks(mdepths, ["%2.2f"%(d) for d in mdepths])
+            plt.legend()
+            plt.ylim(args.yMin, args.yMax)
+
     plt.show()
     #plt.savefig(args.files[0].split('_')[0] + ".png")
