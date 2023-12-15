66d9e4745a1b1983ea539abdd6b44dc02ccbfdbb
Raphael Eckel <raphael.eckel@unifr.ch>
2023-12-15 16:20:22 +0100

    fixed regression test merge

--- examples/ancestralAlleles.sh
+++ examples/ancestralAlleles.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
 
 . $(dirname $0)/find_atlas
-<<<<<<< HEAD
-
-$atlas --task simulate --vcf --type HW --chrLength 100000 --fixedSeed 0 
-$atlas --task alleleCounts --vcf ATLAS_simulations.vcf.gz  --outFormat withAlleles --logFile alleleCounts.out --fixedSeed 0 
-$atlas --task ancestralAlleles --alleleCounts ATLAS_simulations_alleleCounts.txt.gz --logFile ancestralAlleles.out --fixedSeed 0 
-=======
 . $(dirname $0)/simulate_vcf
 
 $atlas --task alleleCounts --vcf ATLAS_simulations.vcf.gz  --outFormat withAlleles \
@@ -15,4 +9,3 @@ $atlas --task alleleCounts --vcf ATLAS_simulations.vcf.gz  --outFormat withAllel
 $atlas --task ancestralAlleles --alleleCounts ATLAS_simulations_alleleCounts.txt.gz \
 	   --minorCountMaximum 2 --totalCountMinimum 4 \
 	   --logFile ancestralAlleles.out --fixedSeed 0
->>>>>>> beta
