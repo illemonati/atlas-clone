6347add6952542ff8ca052d35c79658b6dca6ea6
Andreas <andreas.fueglistaler@protonmail.com>
2024-06-03 13:44:23 +0200

    fixing atlas unit tests

--- VCF/tests/TVcfConverter_Test.cpp
+++ VCF/tests/TVcfConverter_Test.cpp
@@ -92,7 +92,7 @@ public:
 	}
 
 	void writeSampleFile() {
-		TOutputFile sampleFile("test.samples");
+		TOutputFile sampleFile("test.samples", {"samples"});
 		for (auto &it : samplesToKeep) { sampleFile.writeln(it); }
 		// add to parameters
 		parameters().add("samples", "test.samples");
