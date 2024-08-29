492367c66eef6ad939072e2f2894375548b5a8d6
Andreas <andreas.fueglistaler@protonmail.com>
2024-08-29 11:07:41 +0200

    remove sexEstimation script

--- examples/sexEstimation.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-. $(dirname $0)/find_atlas
-. $(dirname $0)/simulate --fixedSeed 222
-
-out="sexEstimation"
-$atlas --task sexEstimation --wholeGenome --window 4567 \
-	   --bam simulate.bam --fasta simulate.fasta \
-	   --fixedSeed 221 --out $out --logFile $out.out 2> $out.eout
