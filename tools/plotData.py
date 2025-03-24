5bb363862a2f43ab0d6e0d5d96cc15e0c9af015f
Andreas <andreas.fueglistaler@protonmail.com>
2025-03-24 08:29:40 +0100

    plotData with multiple files

--- tools/plotData.py
+++ tools/plotData.py
@@ -16,36 +16,58 @@ col  = tol_bright
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Plot columns of table")
-    parser.add_argument("file", type=argparse.FileType('r'), nargs='?', default=sys.stdin, help="Transitions File")
+    parser.add_argument("files", nargs='*', help="Files")
     parser.add_argument("-x", type=int, default=1, help="x-column")
     parser.add_argument("-y", nargs='+', type=int, default=[2], help="y-column")
     parser.add_argument("--yMax", type=float, default=0., help="x-column")
+    parser.add_argument("--xMax", type=float, default=0., help="x-column")
     parser.add_argument("--logx", action="store_true", help="logx")
     parser.add_argument("--logy", action="store_true", help="logy")
     parser.add_argument("--vLine", "-v", type=int)
     parser.add_argument("--format", "-f", default="-")
     parser.add_argument("--grep", "-g", default="")
+    parser.add_argument("--relative",  "-r", action="store_true")
 
     args = parser.parse_args()
-    header = args.file.readline().split()
 
-    lines = filter(lambda line: args.grep in line, args.file)
-    data = genfromtxt(lines)
-    for y in args.y:
-        plt.plot(data[:, args.x - 1], data[:, y - 1], args.format, label=header[y-1])
+    nFiles = len(args.files)
+    for i, fn in enumerate(args.files):
+        if fn.endswith("gz"):
+            f = gzip.open(fn)
+            flabel = fn.split(".txt.gz")[0].split("/")[-1]
+        else:
+            f = open(fn)
+            flabel = fn.split(".txt")[0].split("/")[-1]
 
-    if args.vLine is not None:
-        plt.axvline(args.vLine, ls=":", color="k")
+        header = f.readline().split()
 
-    if args.logx:
-        plt.xscale("log")
-    if args.logy:
-        plt.yscale("log")
+        lines = filter(lambda line: args.grep in line, f)
+        data = genfromtxt(lines)
+        for y in args.y:
+            label = header[y-1]
+            if nFiles > 1:
+                label = flabel + ":" + label
+            ys = data[:, y - 1]
+            if args.relative:
+                ys /= sum(ys)
+            plt.plot(data[:, args.x - 1], ys, args.format, label=label)
+
+        if args.vLine is not None:
+            plt.axvline(args.vLine, ls=":", color="k")
+
+        if args.logx:
+            plt.xscale("log")
+        if args.logy:
+            plt.yscale("log")
 
 
-    if (args.yMax != 0.):
-        plt.ylim(0, args.yMax)
-    plt.legend()
+        if (args.yMax != 0.):
+            plt.ylim(0, args.yMax)
+        if (args.xMax != 0.):
+            plt.xlim(0, args.xMax)
+        plt.legend()
+        f.close()
+
     plt.xlabel(header[args.x-1])
 
     plt.show()
