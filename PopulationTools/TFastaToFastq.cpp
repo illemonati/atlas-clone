7bf1a7c61fdd3340ede2a940f3ae12c10736a708
Xenia Wietlisbach <xenia.wietlisbach@unifr.ch>
2025-04-28 11:34:43 +0200

    toFastq mode done, toBam mode not done

--- PopulationTools/TFastaToFastq.cpp
+++ PopulationTools/TFastaToFastq.cpp
@@ -6,6 +6,7 @@
 #include "coretools/Files/TOutputFile.h"
 #include "genometools/GenomePositions/TGenomeWindow.h"
 #include "genometools/GenomePositions/TGenomePosition.h"
+#include "TBamFile.h"
 
 
 
@@ -13,87 +14,97 @@ using coretools::instances::logfile;
 using coretools::instances::parameters;
 
 
-
 namespace PopulationTools {
+
+    void writeFastqEntry(coretools::TOutputFile& out, const genometools::impl::TGenomeWindowWithInfo<double>& b, const genometools::TFastaReader& reference, size_t start, size_t seq_length) {
+    
+        out.writeln("@", reference.chrName(b.refID()), "_", start, "_", start+seq_length, "_", b.info()); // first thing we actually need for the fastq, but we need to add more info
+        out.writeln(reference.view(b.refID(), start, seq_length));
+        out.writeln("+");
+        out.writeln(std::string(seq_length, 'F'));
+    }
+
+
+    TReadCenterAlignedBase::TReadCenterAlignedBase() : TBamReadTraverser<ReadType::Parsed>() {};
+
+    void TReadCenterAlignedBase::_handleAlignment(BAM::TAlignment& alignment) {
+
+};
     void TFastaToFastq::run(){
-        const auto bedfile=parameters().get("bed"); 
-        const auto fastafile=parameters().get("fasta"); 
-        const auto flank=parameters().get("flanking_length", 100);
-        const auto outname=parameters().get("out", "ATLAS_fastaToFastq");
 
-
-        genometools::TFastaReader reference(fastafile); 
-        genometools::TBedWithInfo<double> bed(bedfile, reference.chromosomes()); 
-
-        coretools::TOutputFile out(outname + ".fastq", "");
-
-
-        //logfile().list(bed.size());
-        //logfile().list(bed.length());
-        //logfile().list(bed[0][0]);
-        //logfile().list(bed[0][1]);
-        //logfile().list(bed[1][0]);
-
-
-        //out.writeln("the first three bases");
-        //out.writeln(reference(0,0));
-        //out.writeln(reference(0,1));
-        //out.writeln(reference(0,2));
-        //out.writeln(reference.view(0,0,2)); // this works
-
-        coretools::TTimer timer;
-        constexpr size_t dCounter = 100000;
-	    size_t counter            = 0;
-	    size_t skip_counter       = 0;
-	    size_t nextPrint          = dCounter;
-
-
-        for (const auto& b: bed) {
-            b.refID();
-            //b.pos(); // I don't think that exists, because b is a GenomeWindow instead of a GenomePosition
-
-            b.size(); // we can check if the length of the bed thing is, so we can exclude all the ones that are not one
-
-
-            // genotype position instead of genotype window?
-            //genometools::TGenomePosition center(b.refID(), b.fromOnChr());
-            //out.writeln("check if center works");
-            //out.writeln(reference[center]); // works
-
-            //out.writeln(reference(b.refID(), b.fromOnChr())); // this works and gives me the correct starting base
-            //out.writeln(reference.view(b)); // this also works but with spaces
-
-            if (flank > b.fromOnChr()) {
-                ++skip_counter;
-                continue;
-            }
-            ++counter;
-
-            const size_t start = b.fromOnChr() - flank;
-            const size_t count = 2*flank + 1;
-
-            // look for max from 0 or start and use that, although you then have maybe shorter region
-            // 
-
-
-            out.writeln("@", bed.name(b.refID()), "_", start, "-", start+count); // first thing we actually need for the fastq, but we need to add more info
-
-
-            out.writeln(reference.view(b.refID(), start, count));
-            
-            //out.writeln(reference(0,0));
-            out.writeln("+");
-            out.writeln(std::string(count, 'F'));
-
-            // report progress
-		    if (counter >= nextPrint) {
-                logfile().list("Parsed ", nextPrint, " positions in ", timer.formattedTime(), ".");
-                nextPrint += dCounter;
-		    }
-
+        const auto mode=parameters().get("mode");
+        if (mode != "toFastq" && mode != "toBed") {
+            UERROR("Unknown mode '", mode, "'! Accepted values are 'toFastq' and 'toBed'.");
         }
 
-        logfile().list("Removed ", skip_counter, " line(s) at the beginning of a chromosome.");
+        const auto fastaname=parameters().get("fasta"); 
+        const auto flank=parameters().get("flank", 100);
+        const auto outname=parameters().get("out", "ATLAS_fastaToFastq");
+
+        genometools::TFastaReader reference(fastaname); 
+
+        if (mode == "toFastq") {
+        
+            const auto bedname=parameters().get("bed"); 
+            genometools::TBedWithInfo<double> bed(bedname, reference.chromosomes()); // reading correct refID from reference
+
+            coretools::TOutputFile out(outname + ".fastq", "");
+
+
+            coretools::TTimer timer;
+            constexpr size_t dCounter = 100000;
+            size_t counter            = 0;
+            size_t skip_counter       = 0;
+            size_t nextPrint          = dCounter;
+
+            const size_t seq_length = 2*flank + 1;
+
+
+            for (const auto& b: bed) {
+
+                // for every position within a range, write one fastq sequence
+                for (size_t i = 0; i < b.size(); ++i) {
+                    const int start = b.fromOnChr() + i - flank;
+                    if (start >= 0) {
+                        writeFastqEntry(out, b, reference, start, seq_length); 
+                    } else {
+                        ++skip_counter;
+                        continue; // instead of skipping, go for max bt 0 and start-flank? 
+
+                    }
+                }
+                
+                // report progress
+                if (counter >= nextPrint) {
+                    logfile().list("Parsed ", nextPrint, " positions in ", timer.formattedTime(), ".");
+                    nextPrint += dCounter;
+                }
+
+            }
+
+            logfile().list("Removed ", skip_counter, " position(s) at the beginning of a chromosome.");
+
+        } else {
+            
+            const auto bamname=parameters().get("bam");
+            genometools::TBedWithInfo<double> out;
+
+
+
+
+            bam = std::make_unique<BAM::TBamFile>(bamname, 0); // not sure what the ID is
+            BAM::TAlignment alignment;
+
+
+
+
+            logfile().list("Parsed length: ", alignment.parsedLength()); // I want to check this at some point bc I'm not sure this will always be 201
+
+
+
+            out.write(outname + ".bed");
+
+        }
 
 
     }
--- PopulationTools/TFastaToFastq.h
+++ PopulationTools/TFastaToFastq.h
@@ -1,8 +1,21 @@
 #ifndef TFASTATOFASTQ_H_
 #define TFASTATOFASTQ_H_
 
+#include "TBamTraverser.h"
+
+
 namespace PopulationTools {
+
 struct TFastaToFastq {
+	void run();
+}
+
+class TReadCenterAlignedBase : public TBamReadTraverser<ReadType::Parsed> {
+	private: 
+		void _handleAlignment(BAM::TAlignment& alignment);
+
+	public:
+	TReadCenterAlignedBase();
 	void run();
 };
 } // namespace PopulationTools
