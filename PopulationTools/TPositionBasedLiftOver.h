acc54c75db479626e0a748d01cef90bcc32f4a83
Daniel Wegmann <daniel.wegmann@unifr.ch>
2025-04-29 13:18:17 +0200

    added TPositionBasedLiftOver files

---
+++ PopulationTools/TPositionBasedLiftOver.h
@@ -0,0 +1,41 @@
+#ifndef TFASTATOFASTQ_H_
+#define TFASTATOFASTQ_H_
+
+#include "TBamTraverser.h"
+#include "genometools/TBed.h"
+
+
+namespace PopulationTools {
+
+class TBedToFastq {
+public:
+	TBedToFastq(){};
+	void run();
+};
+
+class TBamToBed : public GenomeTasks::TBamReadTraverser<GenomeTasks::ReadType::Parsed> {
+private: 
+	genometools::TBedWithInfo<std::string> _outBed;
+	size_t _numPosNotAligned;
+	
+
+	void _handleAlignment(BAM::TAlignment& alignment) override;
+
+public:
+	TBamToBed(){};
+	void run();
+};
+
+
+class TPositionBasedLiftOver{
+public:
+	TPositionBasedLiftOver(){};
+	void run();
+
+};
+
+
+
+} // namespace PopulationTools
+
+#endif
